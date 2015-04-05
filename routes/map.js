2bedd6512bdc9af18725ff28d617886acb2ce484
ALIZA STONE <amstone326@gmail.com>
2015-04-05 16:41:24 -0400

    1C_rate.ejs added, structure for all routes added to app.js and map.js

--- routes/map.js
+++ routes/map.js
@@ -13,3 +13,76 @@ exports.rate1A = function(req, res) {
 exports.rate1B = function(req, res) {
 	res.render('1B_rate', {title:"Map"});
 };
+
+exports.rate1C = function(req, res) {
+	res.render('1C_rate', {title:"Map"});
+};
+
+exports.rate2A = function(req, res) {
+	res.render('2A_rate', {title:"Map"});
+};
+
+exports.rate3A = function(req, res) {
+	res.render('3A_rate', {title:"Map"});
+};
+
+exports.rate4A = function(req, res) {
+	res.render('4A_rate', {title:"Map"});
+};
+
+exports.rate5A = function(req, res) {
+	res.render('5A_rate', {title:"Map"});
+};
+
+exports.rate5B = function(req, res) {
+	res.render('5B_rate', {title:"Map"});
+};
+
+exports.rate6A = function(req, res) {
+	res.render('6A_rate', {title:"Map"});
+};
+
+exports.rate6B = function(req, res) {
+	res.render('6B_rate', {title:"Map"});
+};
+
+exports.status1A = function(req, res) {
+	res.render('1A_status', {title:"Map"});
+};
+
+exports.status1B = function(req, res) {
+	res.render('1B_status', {title:"Map"});
+};
+
+exports.status1C = function(req, res) {
+	res.render('1C_status', {title:"Map"});
+};
+
+exports.status2A = function(req, res) {
+	res.render('2A_status', {title:"Map"});
+};
+
+exports.status3A = function(req, res) {
+	res.render('3A_status', {title:"Map"});
+};
+
+exports.status4A = function(req, res) {
+	res.render('4A_status', {title:"Map"});
+};
+
+exports.status5A = function(req, res) {
+	res.render('5A_status', {title:"Map"});
+};
+
+exports.status5B = function(req, res) {
+	res.render('5B_status', {title:"Map"});
+};
+
+exports.status6A = function(req, res) {
+	res.render('6A_status', {title:"Map"});
+};
+
+exports.status6B = function(req, res) {
+	res.render('6B_status', {title:"Map"});
+};
+
